5d25aa8b8ac941038e858737b8f4e8bcde16f29d
Pepe <pepoclesng@gmail.com>
2022-10-27 21:01:36 -0500

    solving small mistake with jwt

--- src/functions/VerifyJWT.ts
+++ src/functions/VerifyJWT.ts
@@ -2,17 +2,19 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 const VerifyJWT = async (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers['authorization'];
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) return res.status(200).send({ success: false, message: "No token was provided" });
   if (!process.env.TOKEN_SECRET) return res.status(500).send({ success: false, message: "Internal server error" });
 
   try {
-    const verification = jwt.verify(token, process.env.TOKEN_SECRET);
-    next();
+    jwt.verify(token, process.env.TOKEN_SECRET);
   } catch (error) {
     return res.status(400).send(error);
   }
+
+  next();
 };
 
 export default VerifyJWT;
