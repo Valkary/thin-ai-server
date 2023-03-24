305b6a025e8a919f52922d764f12cf6bc9f1204e
Pepe <pepoclesng@gmail.com>
2023-03-24 13:21:42 -0600

    just laying out the project structure

--- src/Router.ts
+++ src/Router.ts
@@ -5,10 +5,7 @@ import multer from "multer";
 import VerifyJWT from "./functions/VerifyJWT";
 
 /* CONTROLLERS */
-import PatientController from "./controllers/PatientController";
 import UserController from "./controllers/UserController";
-import AppointmentController from "./controllers/AppointmentController";
-import FileController from "./controllers/FileController";
 
 const AppRouter = Router();
 const upload = multer({ dest: "uploads/" });
@@ -20,20 +17,6 @@ AppRouter.get('/', (_, res) => {
 
 /* USERS */
 AppRouter.get('/users/get_all', VerifyJWT, UserController.get_all);
-AppRouter.post('/users/create', VerifyJWT, UserController.create);
-AppRouter.post('/users/update_pass', VerifyJWT, UserController.update_password);
-AppRouter.post('/users/login', UserController.login);
 
-/* PATIENTS */
-AppRouter.get('/patients/get_all', VerifyJWT, PatientController.get_all);
-AppRouter.post('/patients/create', VerifyJWT, PatientController.create);
-
-/* APPOINTMENTS */
-AppRouter.get('/appointments/get_all', VerifyJWT, AppointmentController.get_all);
-AppRouter.post('/appointments/create', VerifyJWT, AppointmentController.create);
-
-/* FILES */
-AppRouter.get('/files/get_all', VerifyJWT, FileController.get_all);
-AppRouter.post('/files/upload_pdf', VerifyJWT, upload.array("files"), FileController.upload_pdf);
 
 export default AppRouter;
--- src/types/Patient.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const post_patient_schema = z.object({
-  first_names: z.string({ required_error: "Invalid first names" }),
-  last_names: z.string({ required_error: "Invalid last names" })
-});
-
-export type Patient = z.infer<typeof post_patient_schema>;
